17de09a932b3f6ce91c3924c0069a3239494402d
jaimeelynn <jaime@captain.tools>
2022-11-07 06:23:07 -0500

    add tracker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,10 @@ import '@styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import type { AppProps } from 'next/app'
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-170103870-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
