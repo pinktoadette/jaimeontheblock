f7ae0e689df3437f7ca66b99607c1abf7221cda5
jaimeelynn <jaime@captain.tools>
2022-11-08 08:51:00 -0500

    animate points

--- components/Achievements.tsx
+++ components/Achievements.tsx
@@ -1,46 +1,66 @@
 import Container from 'react-bootstrap/Container';
 
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import list from "./utilities/achievement.json";
 import { Card } from 'react-bootstrap';
-interface AList { item: { title: string; subtitle: string; }; key: number; }
-
-const CardList = ( {item: ele} : AList) => {
-  
-    return (
-      <motion.div
-        className="card-container"
-        initial="offscreen"
-        whileInView="onscreen"
-        viewport={{ once: true, amount:1 }}
-      >
-        <motion.div>
-          <Card className="alist">
-            <Card.Body> 
-                
-          <h3>{ele.title}</h3>
-          <span className="text-muted">{ele.subtitle}</span>
-            </Card.Body>
-          </Card>
-        </motion.div>
-      </motion.div>
-    );
+import { useRef } from 'react';
+interface AList { item: { title: string; subtitle: string; } }
+const cardVariants: Variants = {
+  offscreen: {
+    y: 500
+  },
+  onscreen: {
+    y: 0,
+    transition: {
+      type: "spring",
+      bounce: 0.4,
+      duration: 0.8
+    }
   }
+};
 
-  
+const CardList = ({ item: ele }: AList) => {
+  return (
+    <Card className="alist">
+      <Card.Body>
+        <h3>{ele.title}</h3>
+        <span className="text-muted">{ele.subtitle}</span>
+      </Card.Body>
+    </Card>
+  );
+}
+
+
 const AchievementList = () => {
-    
-    return (
-      <section>
+  
+  return (
+    <section>
       <Container>
         <h3>Execute. Deliver. Lead. Pioneer.</h3>
-        {list.map((item, idx) => (
-            <CardList  item={item} key={idx} />
-          ))}
+
+        {list.map((item, i) => (
+        <motion.div
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+          key={i}
+          animate={{ opacity: 1,
+            translateX: i *15,
+            translateY: 0 }}
+          transition={{ duration: 0.3, delay: i }}
+        > 
+        <motion.div variants={cardVariants}>
+        <CardList item={item} />
+      </motion.div>
+      
+        </motion.div>
+      ))}
+
+
       </Container>
 
-      </section>
-    );
+    </section>
+  );
 }
 
 
